90fdb99c0fa957cb0b1ee6f1e388e3152fd29104
raderleao <raderleao@hotmail.com>
2023-03-09 22:54:25 -0300

    add give

--- application/src/main/java/com/raderleao/admin/catalogo/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/com/raderleao/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -1,7 +1,9 @@
 package com.raderleao.admin.catalogo.application.category.create;
 
 import com.raderleao.admin.catalogo.application.UseCase;
+import com.raderleao.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
 public abstract class CreateCategoryUseCase
-    extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+    extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 }
--- application/src/main/java/com/raderleao/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/raderleao/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
@@ -2,7 +2,8 @@ package com.raderleao.admin.catalogo.application.category.create;
 
 import com.raderleao.admin.catalogo.domain.category.Category;
 import com.raderleao.admin.catalogo.domain.category.CategoryGateway;
-import com.raderleao.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
+import com.raderleao.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
 public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
 
@@ -13,14 +14,19 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
     }
 
     @Override
-    public CreateCategoryOutput execute(final CreateCategoryCommand aCommand) {
+    public Either<Notification, CreateCategoryOutput> execute(final CreateCategoryCommand aCommand) {
         final var aName = aCommand.name();
         final var aDescription = aCommand.description();
         final var isActive = aCommand.isActive();
 
-        final var aCategory = Category.newCategory(aName,aDescription, isActive);
-        aCategory.validate(new ThrowsValidationHandler());
+        final var notification = Notification.create();
 
+        final var aCategory = Category.newCategory(aName,aDescription, isActive);
+        aCategory.validate(notification);
+
+        if (notification.hasError()) {
+            //
+        }
         return CreateCategoryOutput.from(this.categoryGateway.create(aCategory));
 
     }
