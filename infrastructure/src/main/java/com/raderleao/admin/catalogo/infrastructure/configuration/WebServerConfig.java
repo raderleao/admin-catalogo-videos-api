f7cf017b7021c1d51d04cd1e59d2468a6dcdb2a5
raderleao <raderleao@hotmail.com>
2023-03-14 18:20:08 -0300

    Spring Configuration

--- infrastructure/src/main/java/com/raderleao/admin/catalogo/infrastructure/configuration/WebServerConfig.java
+++ infrastructure/src/main/java/com/raderleao/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -1,6 +1,5 @@
 package com.raderleao.admin.catalogo.infrastructure.configuration;
 
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
