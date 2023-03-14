f051ee1a87a0df62bfde923bcc77d4a20aa9d0aa
raderleao <raderleao@hotmail.com>
2023-03-14 13:56:26 -0300

    Spring Configuration

--- infrastructure/src/main/java/com/raderleao/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/raderleao/admin/catalogo/infrastructure/Main.java
@@ -1,11 +1,16 @@
 package com.raderleao.admin.catalogo.infrastructure;
 
-import com.raderleao.admin.catalogo.application.UseCase;
+import com.raderleao.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
 
     }
 }
