28e7755b2ec7d49a0fdfbdd8baec414c266258aa
raderleao <raderleao@hotmail.com>
2023-03-17 16:22:42 -0300

    injecao de casos de uso

--- infrastructure/src/test/java/com/raderleao/admin/catalogo/application/category/create/CreateCategoryUseCaseIT.java
+++ infrastructure/src/test/java/com/raderleao/admin/catalogo/application/category/create/CreateCategoryUseCaseIT.java
@@ -1,2 +1,44 @@
-package com.raderleao.admin.catalogo.application.category.create;public class CreateCategoryUseCaseIT {
+package com.raderleao.admin.catalogo.application.category.create;
+
+import com.raderleao.admin.catalogo.IntegrationTest;
+import com.raderleao.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class CreateCategoryUseCaseIT {
+
+    @Autowired
+    private CreateCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+
+        final var expectedName = "Filme";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var aCommand =
+                CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualOutPut = useCase.execute(aCommand).get();
+        Assertions.assertNotNull(actualOutPut);
+        Assertions.assertNotNull(actualOutPut.id());
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        final var actualCategory =
+                categoryRepository.findById(actualOutPut.id().getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
 }
