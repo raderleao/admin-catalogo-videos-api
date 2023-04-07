118c8b04ee881d170614c4a8fcb4810728d83103
raderleao <raderleao@hotmail.com>
2023-04-07 00:44:11 -0300

    Endpoint de criacao de genre e seus testes integrados

---
+++ infrastructure/src/main/java/com/raderleao/admin/catalogo/infrastructure/api/controllers/GenreController.java
@@ -0,0 +1,63 @@
+package com.raderleao.admin.catalogo.infrastructure.api.controllers;
+
+import com.raderleao.admin.catalogo.application.genre.create.CreateGenreCommand;
+import com.raderleao.admin.catalogo.application.genre.create.CreateGenreUseCase;
+import com.raderleao.admin.catalogo.domain.pagination.Pagination;
+import com.raderleao.admin.catalogo.infrastructure.api.GenreAPI;
+import com.raderleao.admin.catalogo.infrastructure.genre.models.CreateGenreRequest;
+import com.raderleao.admin.catalogo.infrastructure.genre.models.GenreListResponse;
+import com.raderleao.admin.catalogo.infrastructure.genre.models.GenreResponse;
+import com.raderleao.admin.catalogo.infrastructure.genre.models.UpdateGenreRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+public class GenreController implements GenreAPI {
+
+    private final CreateGenreUseCase createGenreUseCase;
+
+    public GenreController(final CreateGenreUseCase createGenreUseCase) {
+        this.createGenreUseCase = createGenreUseCase;
+    }
+
+    @Override
+    public ResponseEntity<?> create(final CreateGenreRequest input) {
+        final var aCommand = CreateGenreCommand.with(
+                input.name(),
+                input.isActive(),
+                input.categories()
+        );
+
+        final var output = this.createGenreUseCase.execute(aCommand);
+
+        return ResponseEntity.created(URI.create("/genres/" + output.id())).body(output);
+    }
+
+    @Override
+    public Pagination<GenreListResponse> list(
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction
+    ){
+        return null;
+    }
+
+    @Override
+    public GenreResponse getById(final String id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> updateById(final String id, final UpdateGenreRequest input) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(final String id) {
+
+    }
+}
